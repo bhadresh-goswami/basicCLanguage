70b73669dcd7e9d24330ede92f1182ff9ef9f1fb
tops-bhadresh <tops.bhadresh@gmail.com>
2018-11-29 04:01:54 -0800

    first commit

---
+++ basicCLanguage/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  basicCLanguage
+//
+//  Created by Mac on 29/11/18.
+//  Copyright © 2018 tops. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hi!\n");
+    return 0;
+}
